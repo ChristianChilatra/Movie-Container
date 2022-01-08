211f4d6d723ea14d69ae87c6f6a195c7cc678c88
Christian Chilatra <chilatra1998@gmail.com>
2022-01-08 01:50:17 -0500

    No Comment

--- src/services/api.js
+++ src/services/api.js
@@ -3,7 +3,7 @@ class API{
     this.KEY_API = API_KEY;
   }
 
-  baseAPI = 'https://api.themoviedb.org/3/'
+  baseAPI = 'http://api.themoviedb.org/3/'
 
   discoverMovie(page){
     return `${this.baseAPI}discover/movie/?api_key=${this.KEY_API}&page=${page}`;

cbfe8eeb92f0f232dc6804ca7699d8749aa714ad
Christian David Chilatra Mendez <37522628+ChristianChilatra@users.noreply.github.com>
2022-01-13 19:20:20 -0500

    Finalización reproducción tráiler

--- src/services/api.js
+++ src/services/api.js
@@ -5,18 +5,26 @@ class API {
 
   baseAPI = "https://api.themoviedb.org/3/";
 
-  discoverMovie(page,filter, query, id ) {
-    switch (filter) {
+  discoverMovie(params) {
+
+    const setParams = {
+      filter: params? (params.filter?params.filter:'') : '',
+      page: params? (params.page?params.page:1) : 1,
+      query: params? (params.query?params.query:'') : '',
+      id: params? (params.id?params.id:0) : 0
+    }
+
+    switch (setParams.filter) {
       case "asc":
-        return `${this.baseAPI}discover/movie?api_key=${this.KEY_API}&sort_by=vote_average.asc&page=${page}&language=es`;
+        return `${this.baseAPI}discover/movie?api_key=${this.KEY_API}&sort_by=vote_average.asc&page=${setParams.page}&language=es`;
       case "search":
-        return `${this.baseAPI}search/movie?api_key=${this.KEY_API}&query=${query}&page=${page}&language=es`;
+        return `${this.baseAPI}search/movie?api_key=${this.KEY_API}&query=${setParams.query}&page=${setParams.page}&language=es`;
       case "desc":
-        return `${this.baseAPI}discover/movie?api_key=${this.KEY_API}&sort_by=vote_average.desc&page=${page}&language=es`;
+        return `${this.baseAPI}discover/movie?api_key=${this.KEY_API}&sort_by=vote_average.desc&page=${setParams.page}&language=es`;
       case "video":
-        return `${this.baseAPI}movie/${id}/videos?api_key=${this.KEY_API}&language=es`;
+        return `${this.baseAPI}movie/${setParams.id}/videos?api_key=${this.KEY_API}&language=es`;
       default:
-        return `${this.baseAPI}discover/movie?api_key=${this.KEY_API}&page=${page}&language=es`;
+        return `${this.baseAPI}discover/movie?api_key=${this.KEY_API}&page=${setParams.page}&language=es`;
     }
   }
 
